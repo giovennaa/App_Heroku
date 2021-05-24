9b317423abc598b547d7030a6705c25855007a3b
Cimy Júnior <cimy@pop-os.localdomain>
2021-05-24 20:46:22 -0300

    fist commit

---
+++ Array_And_ Methods/app.js
@@ -0,0 +1,28 @@
+//importando o express 
+var express = require('express');
+
+//Criando a var app para chamar os métodos do express
+var app = express();
+
+//setando a view engine => EJS
+app.set('view engine', 'ejs');
+
+//Config path do arquivos estáticos
+app.use(express.static('./public'));
+
+
+app.get('/array', function(req,res){
+    res.render('arrey/arrey.ejs');
+});
+
+app.get('/admin', function(req,res){
+    res.render('admin/form_add_noticia');
+});
+
+app.get('/methods', function(req,res){
+    res.render('methods/methods.ejs');
+});
+
+app.listen(9001, function(){
+    console.log("Servidor rodando com Express");
+});
